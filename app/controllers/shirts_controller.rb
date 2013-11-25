279d3f6f36f8a3d5998892c0e21eb7020173fcbc
Steven Nunez <snunez@cyrusinnovation.com>
2013-11-25 17:06:33 -0500

    Update to allow creation of new shirts

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -2,4 +2,29 @@ class ShirtsController < ApplicationController
   def index
     @shirts = Shirt.all
   end
+
+  def new
+    @shirt = Shirt.new
+  end
+
+  def create
+    safe_shirt = params.require(:shirt).permit(:name, :description, :image)
+    shirt = Shirt.create safe_shirt
+    redirect_to shirt
+  end
+
+  def show
+    @shirt = Shirt.find params[:id]
+  end
+
+  def edit
+    @shirt = Shirt.find params[:id]
+  end
+
+  def update
+    @shirt = Shirt.find params[:id]
+    safe_shirt = params.require(:shirt).permit(:name, :description, :image)
+    @shirt.update safe_shirt
+    redirect_to @shirt
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 ShirtsApp::Application.routes.draw do
   root "shirts#index"
+  # these are special :index, :new, :create, :edit, :update, :destroy, :show
+  resources :shirts, only: [:index, :new, :create, :show, :edit, :update]
+  get "search", to: "search#index"
 end
